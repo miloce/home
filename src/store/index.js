7dc4df6143bae3d55a79f48104f07c4ee702564a
miloce <admim@luozhinet.com>
2023-01-21 13:43:53 +0800

    CI

--- src/store/index.js
+++ src/store/index.js
@@ -15,7 +15,7 @@ export const mainStore = defineStore("main", {
             mobileOpenState: false, // 移动端开启状态
             mobileFuncState: false, // 移动端功能区开启状态
             setOpenState: false, // 设置页面开启状态
-            playerState: true, // 当前播放状态
+            playerState: false, // 当前播放状态
             playerTitle: null, // 当前播放歌曲名
             playerArtist: null, // 当前播放歌手名
             playerLrc: "歌词加载中", // 当前播放歌词
