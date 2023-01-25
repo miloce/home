d808f98ce35454ae20f5d9ffa42ba5baa956c8a6
微星 <admin@luozhinet.com>
2023-01-25 12:51:01 +0800

    修复，

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -20,8 +20,8 @@
       >&nbsp;&amp;
       <!-- 网安备案 -->
      <div style="width:300px;margin:0 auto; padding:20px 0;">
-     <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=36010802000802" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="" style="float:left;"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">赣公网安备 36010802000802号</p></a>
-    </div>
+     <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=36010802000802" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="" style="float:left;"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">赣公网安备 36010802000802号</p></a
+     >
     </div>
     <div class="lrc" v-show="store.playerState">
       <music-one theme="filled" size="18" fill="#efefef" />
