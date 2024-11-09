06468b8235f4320a4b71ad92a40d62c2a1852494
微星 <microne@qq.com>
2024-11-09 21:04:29 +0800

    Update index.vue

--- src/components/Background/index.vue
+++ src/components/Background/index.vue
@@ -29,10 +29,6 @@ const changeBg = (type) => {
     )}.webp`;
   } else if (type == 1) {
     bgUrl.value = "https://api.dujin.org/bing/1920.php";
-  } else if (type == 2) {
-    bgUrl.value = "https://api.ixiaowai.cn/gqapi/gqapi.php";
-  } else if (type == 3) {
-    bgUrl.value = "https://api.ixiaowai.cn/api/api.php";
   }
 };
 
