123e153222177068577ddb1b49ad905f823c938f
微星 <microne@qq.com>
2024-11-09 21:25:27 +0800

    Update index.vue

--- src/components/Weather/index.vue
+++ src/components/Weather/index.vue
@@ -1,12 +1,15 @@
 <template>
-  <div
-    class="weather"
-    v-if="weatherData.adCode.city && weatherData.weather.weather"
-  >
+  <div class="weather" v-if="weatherData.adCode.city && weatherData.weather.weather">
     <span>{{ weatherData.adCode.city }}&nbsp;</span>
     <span>{{ weatherData.weather.weather }}&nbsp;</span>
     <span>{{ weatherData.weather.temperature }}℃</span>
-    <span class="sm-hidden">&nbsp;{{ weatherData.weather.winddirection }}风&nbsp;</span>
+    <span class="sm-hidden">
+      &nbsp;{{
+        weatherData.weather.winddirection?.endsWith("风")
+          ? weatherData.weather.winddirection
+          : weatherData.weather.winddirection + "风"
+      }}&nbsp;
+    </span>
     <span class="sm-hidden">{{ weatherData.weather.windpower }}&nbsp;级</span>
   </div>
   <div class="weather" v-else>
@@ -15,15 +18,14 @@
 </template>
 
 <script setup>
-import { onMounted, reactive, h } from "vue";
-import { getAdcode, getWeather } from "@/api";
+import { getAdcode, getWeather, getOtherWeather } from "@/api";
 import { Error } from "@icon-park/vue-next";
 
 // 高德开发者 Key
-let mainKey = import.meta.env.VITE_WEATHER_KEY;
+const mainKey = import.meta.env.VITE_WEATHER_KEY;
 
 // 天气数据
-let weatherData = reactive({
+const weatherData = reactive({
   adCode: {
     city: null, // 城市
     adcode: null, // 城市编码
@@ -36,47 +38,67 @@ let weatherData = reactive({
   },
 });
 
+// 取出天气平均值
+const getTemperature = (min, max) => {
+  try {
+    // 计算平均值并四舍五入
+    const average = (Number(min) + Number(max)) / 2;
+    return Math.round(average);
+  } catch (error) {
+    console.error("计算温度出现错误：", error);
+    return "NaN";
+  }
+};
+
 // 获取天气数据
-const getWeatherData = () => {
-  // 获取地理位置信息
-  if (!mainKey) return onError("请配置天气 Key");
-  getAdcode(mainKey)
-    .then((res) => {
-      if (res.status) {
-        weatherData.adCode = {
-          city: res.city,
-          adcode: res.adcode,
-        };
-        // 获取天气信息
-        getWeather(mainKey, weatherData.adCode.adcode)
-          .then((res) => {
-            if (res.status) {
-              weatherData.weather = {
-                weather: res.lives[0].weather,
-                temperature: res.lives[0].temperature,
-                winddirection: res.lives[0].winddirection,
-                windpower: res.lives[0].windpower,
-              };
-            } else {
-              onError("天气信息获取失败");
-            }
-          })
-          .catch(() => {
-            onError("天气信息获取失败");
-          });
-      } else {
-        onError("地理位置获取失败");
+const getWeatherData = async () => {
+  try {
+    // 获取地理位置信息
+    if (!mainKey) {
+      console.log("未配置，使用备用天气接口");
+      const result = await getOtherWeather();
+      console.log(result);
+      const data = result.result;
+      weatherData.adCode = {
+        city: data.city.City || "未知地区",
+        // adcode: data.city.cityId,
+      };
+      weatherData.weather = {
+        weather: data.condition.day_weather,
+        temperature: getTemperature(data.condition.min_degree, data.condition.max_degree),
+        winddirection: data.condition.day_wind_direction,
+        windpower: data.condition.day_wind_power,
+      };
+    } else {
+      // 获取 Adcode
+      const adCode = await getAdcode(mainKey);
+      console.log(adCode);
+      if (adCode.infocode !== "10000") {
+        throw "地区查询失败";
       }
-    })
-    .catch(() => {
-      onError("地理位置获取失败");
-    });
+      weatherData.adCode = {
+        city: adCode.city,
+        adcode: adCode.adcode,
+      };
+      // 获取天气信息
+      const result = await getWeather(mainKey, weatherData.adCode.adcode);
+      weatherData.weather = {
+        weather: result.lives[0].weather,
+        temperature: result.lives[0].temperature,
+        winddirection: result.lives[0].winddirection,
+        windpower: result.lives[0].windpower,
+      };
+    }
+  } catch (error) {
+    console.error("天气信息获取失败:" + error);
+    onError("天气信息获取失败");
+  }
 };
 
 // 报错信息
 const onError = (message) => {
   ElMessage({
-    message: message,
+    message,
     icon: h(Error, {
       theme: "filled",
       fill: "#efefef",
